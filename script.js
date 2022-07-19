690537f4a369cce49f5ba9f27e82993cbb84939d
bayani482 <103539309+bayani482@users.noreply.github.com>
2022-07-18 21:58:15 -0700

    Controller 2.0 working
    need to add labels to meters and pictures

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+//keyboard & mouse
 document.addEventListener("keydown",function(event){
     event.preventDefault();
     var key;
@@ -125,7 +126,6 @@ document.addEventListener('keyup', function(event) {
     pressed.classList.remove('press');
     pressed.classList.add('active');
 });
-
 var OSName
 if (navigator.userAgent.indexOf("Win") !=-1) {
     OSName="Windows";
@@ -139,7 +139,6 @@ else if (navigator.userAgent.indexOf("X11") !=-1) {
 else if (navigator.userAgent.indexOf("Linux") !=-1) {
     OSName="Linux";
 }
-
 var startkey="notsure";
 if (OSName==="Windows") startkey="windows";
 if (OSName==="MacOS") startkey="command";
@@ -156,7 +155,7 @@ var backkey="notsure";
 if (OSName==="Windows") backkey="backspace"
 if (OSName==="MacOS") backkey="delete";
 if (OSName==="UNIX") backkey="delete";
-
+//Mouse
 var resetActive = document.getElementsByClassName('active');
 var resetPressed = document.getElementsByClassName('press');
 
@@ -168,6 +167,117 @@ while(resetPressed.length > 0 ){
     resetPressed[0].classList.remove('press');
 }
 }
+//controller
+// Get the state of all gamepads
+var haveEvents = 'GamepadEvent' in window;
+var haveWebkitEvents = 'WebKitGamepadEvent' in window;
+var controllers = {};
+var rAF = window.mozRequestAnimationFrame ||
+  window.webkitRequestAnimationFrame ||
+  window.requestAnimationFrame;
 
+function connecthandler(e) {
+  addgamepad(e.gamepad);
+}
+function addgamepad(gamepad) {
+  controllers[gamepad.index] = gamepad; var d = document.createElement("div");
+  d.setAttribute("id", "controller" + gamepad.index);
+  var t = document.createElement("h1");
+  t.appendChild(document.createTextNode("Gamepad: " + gamepad.id));
+  d.appendChild(t);
+  var b = document.createElement("div");
+  b.className = "buttons";
+  for (var i=0; i<gamepad.buttons.length; i++) {
+    var e = document.createElement("span");
+    e.className = "button";
+    e.id = 'b' + i;
+    e.innerHTML = 'B'+i;
+    b.appendChild(e);
+  }
+  d.appendChild(b);
+  var a = document.createElement("div");
+  a.className = "axes";
+  for (i=0; i<gamepad.axes.length; i++) {
+    e = document.createElement("meter");
+    e.id = "a" + i;
+    e.title = 'Axis'+ i;
+    e.className = "axis";
+    e.setAttribute("min", "-1");
+    e.setAttribute("max", "1");
+    e.setAttribute("value", "0");
+    a.appendChild(e);
+  }
+  d.appendChild(a);
+  document.getElementById("start").style.display = "none";
+  document.body.appendChild(d);
+  rAF(updateStatus);
+}
 
+function disconnecthandler(e) {
+  removegamepad(e.gamepad);
+}
+
+function removegamepad(gamepad) {
+  var d = document.getElementById("controller" + gamepad.index);
+  document.body.removeChild(d);
+  delete controllers[gamepad.index];
+}
+
+function updateStatus() {
+  scangamepads();
+  for (j in controllers) {
+    var controller = controllers[j];
+    var d = document.getElementById("controller" + j);
+    var buttons = d.getElementsByClassName("button");
+    for (var i=0; i<controller.buttons.length; i++) {
+      var b = buttons[i];
+      var val = controller.buttons[i];
+      var pressed = val == 1.0;
+      var touched = false;
+      if (typeof(val) == "object") {
+        pressed = val.pressed;
+        if ('touched' in val) {
+          touched = val.touched;
+        }
+        val = val.value;
+      }
+      var pct = Math.round(val * 100) + "%";
+      b.style.backgroundSize = pct + " " + pct;
+      b.className = "button";
+      if (pressed) {
+        b.className += " pressed";
+      }
+      if (touched) {
+        b.className += " touched";
+      }
+    }
+
+    var axes = d.getElementsByClassName("axis");
+    for (var i=0; i<controller.axes.length; i++) {
+      var a = axes[i];
+      a.innerHTML = i + ": " + controller.axes[i].toFixed(4);
+      a.setAttribute("value", controller.axes[i]);
+    }
+  }
+  rAF(updateStatus);
+}
+
+function scangamepads() {
+  var gamepads = navigator.getGamepads ? navigator.getGamepads() : (navigator.webkitGetGamepads ? navigator.webkitGetGamepads() : []);
+  for (var i = 0; i < gamepads.length; i++) {
+    if (gamepads[i] && (gamepads[i].index in controllers)) {
+      controllers[gamepads[i].index] = gamepads[i];
+    }
+  }
+}
+
+if (haveEvents) {
+  window.addEventListener("gamepadconnected", connecthandler);
+  window.addEventListener("gamepaddisconnected", disconnecthandler);
+} else if (haveWebkitEvents) {
+  window.addEventListener("webkitgamepadconnected", connecthandler);
+  window.addEventListener("webkitgamepaddisconnected", disconnecthandler);
+} else {
+  setInterval(scangamepads, 500);
+}
 
